c143c0e2263bd46a2d579e6e67324db5a4b17ab1
luskoeqk <lusy2017@gmail.com>
2022-07-21 12:06:40 +0300

    list games

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,21 +6,21 @@ export const Header = () => {
             <header>
                 {/* Navigation */}
                 <h1>
-                    <a className="home" href="#">
+                    <a className="home" href="/">
                         GamesPlay
                     </a>
                 </h1>
                 <nav>
-                    <a href="#">All games</a>
+                    <a href="/">All games</a>
                     {/* Logged-in users */}
                     <div id="user">
-                        <a href="#">Create Game</a>
-                        <a href="#">Logout</a>
+                        <a href="/">Create Game</a>
+                        <a href="/">Logout</a>
                     </div>
                     {/* Guest users */}
                     <div id="guest">
-                        <a href="#">Login</a>
-                        <a href="#">Register</a>
+                        <a href="/">Login</a>
+                        <a href="/">Register</a>
                     </div>
                 </nav>
             </header>
